2682ee2507b09d704df87d6cd95271475161412e
dtorannpu <dtorannpu@gmail.com>
2025-08-10 13:48:24 +0900

    Update ESLint and Prettier configurations; remove unused Home component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,20 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tailwind from "eslint-plugin-tailwindcss";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-    eslintConfigPrettier,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tailwind.configs["flat/recommended"],
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
